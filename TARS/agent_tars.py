1b7340a6e4419d3f63a4374bce8d8e8b52d3b01c
Gustavo Freitas <gprofi98@gmail.com>
2025-05-25 17:42:28 -0300

    First commit

---
+++ TARS/agent_tars.py
@@ -0,0 +1,12 @@
+import io
+import json
+import whisper
+import requests
+import webrtcvad
+import numpy as np
+import sounddevice as sd
+from IPython.display import Audio
+import scipy.io.wavfile as wavfile
+
+
+print('oi')
