3c654f3fd138585d3f622541fc1a73ffaa9ed024
Gustavo Freitas <gprofi98@gmail.com>
2025-05-25 20:20:23 -0300

    Add voice commmand

--- TARS/agent_tars.py
+++ TARS/agent_tars.py
@@ -1,12 +1,1 @@
-import io
-import json
-import whisper
-import requests
-import webrtcvad
-import numpy as np
-import sounddevice as sd
-from IPython.display import Audio
-import scipy.io.wavfile as wavfile
-
-
-print('oi')
+# nothing yet :(
